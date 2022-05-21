24d4ffd6827960a21b160cf5c9b98b5d79bd8dc7
Juston Roney <juston.roney@fanduel.com>
2022-05-21 18:40:31 -0400

    * reusable templates, * auto-populate previous, * CSS tweaks, lint

--- app/main.py
+++ app/main.py
@@ -15,10 +15,24 @@ templates_dir = path / "templates"
 app.mount("/static", StaticFiles(directory=static_path), name="static")
 templates = Jinja2Templates(directory=templates_dir)
 
-overview_prefix = "slides/overview"
+slides_prefix = "slides"
+overview_prefix = f"{slides_prefix}/overview"
+cli_prefix = f"{slides_prefix}/cli_intro"
+bullet_slide = "one_column_no_header_title_block.html"
+two_column_table = "two_column_header_title_block.html"
+three_column_table = "three_column_header_title_block.html"
 
 
-@app.get("/", response_class=HTMLResponse)
+async def get_referer(request: Request) -> str:
+    try:
+        referer: str = f"/{request.headers.get('referer').split('/')[-1]}"
+    except AttributeError:
+        referer = '/title'
+
+    return referer
+
+
+@app.get("/title", response_class=HTMLResponse)
 async def root(request: Request):
     resp: dict = {
         'request': request,
@@ -26,9 +40,10 @@ async def root(request: Request):
         "description": '"A very high-level overview"',
         "sub_title": "A very high-level overview",
         "next": "/agenda",
-        "previous": "/"
+        "previous": await get_referer(request),
     }
-    return templates.TemplateResponse(f'{overview_prefix}/index.html', resp)
+
+    return templates.TemplateResponse(f'{overview_prefix}/title.html', resp)
 
 
 @app.get("/agenda", response_class=HTMLResponse)
@@ -40,15 +55,17 @@ async def agenda(request: Request):
         "Important variables",
         "EPOCH",
     ]
+
     resp: dict = {
         'request': request,
         "title": "Agenda",
         "description": '"Agenda"',
-        "previous": "/",
+        "previous": await get_referer(request),
         "next": "/prim-diff",
         "items": agenda_items,
     }
-    return templates.TemplateResponse(f'{overview_prefix}/agenda.html', resp)
+
+    return templates.TemplateResponse(f'{overview_prefix}/{bullet_slide}', resp)
 
 
 @app.get("/prim-diff", response_class=HTMLResponse)
@@ -74,41 +91,43 @@ async def prim_diff(request: Request):
 
     resp: dict = {
         "request": request,
-        "title": '"Primary Differences"',
+        "title": "Primary Differences",
         "description": "Primary differences between linux and unix",
-        "previous": "/agenda",
+        "previous": await get_referer(request),
         "next": "/shells",
         "thead_one": "Linux",
         "thead_two": "UNIX",
         "items": items,
     }
-    return templates.TemplateResponse(f'{overview_prefix}/prim-diff.html', resp)
+
+    return templates.TemplateResponse(f'{overview_prefix}/{two_column_table}', resp)
 
 
 @app.get("/shells", response_class=HTMLResponse)
 async def shells(request: Request):
-    shells: dict = {
-        "Login": "Initial login shell denoted with a hyphen (e.g. -zsh, -bash)",
-        "Interactive": "Inherits from login shell. Sources user profile(s)",
-        "Non-interactive": "Does not source user profiles. (e.g. call a script from a script)",
-    }
+    items: tuple = (
+        ("Login", "Initial login shell denoted with a hyphen (e.g. -zsh, -bash)"),
+        ("Interactive", "Inherits from login shell. Sources user profile(s)"),
+        ("Non-interactive", "Does not source user profiles. (e.g. call a script from a script)"),
+    )
+
     resp: dict = {
         "request": request,
         "title": "Types of Shells",
         "description": '"System Profile Slide"',
-        "previous": "/prim-diff",
+        "previous": await get_referer(request),
         "next": "/config-files",
         "thead_one": "Shell",
         "thead_two": "Description",
-        "shells": shells,
+        "items": items,
     }
 
-    return templates.TemplateResponse(f'{overview_prefix}/shells.html', resp)
+    return templates.TemplateResponse(f'{overview_prefix}/{two_column_table}', resp)
 
 
 @app.get("/config-files", response_class=HTMLResponse)
 async def config_files(request: Request):
-    config_files: tuple = (
+    items: tuple = (
         (
             '/etc/profile',
             'System-wide profile for ksh, bash, etc.'
@@ -126,17 +145,19 @@ async def config_files(request: Request):
             'User configuration for interactive shells'
         ),
     )
+
     resp: dict = {
         "request": request,
         "title": "Configuration Profiles",
         "description": '"System Profile Slide"',
         "next": "/variables",
-        "previous": "/shells",
+        "previous": await get_referer(request),
         "thead_one": "Profile",
         "thead_two": "Description",
-        "items": config_files
+        "items": items,
     }
-    return templates.TemplateResponse(f'{overview_prefix}/config-files.html', resp)
+
+    return templates.TemplateResponse(f'{overview_prefix}/{two_column_table}', resp)
 
 
 @app.get("/variables", response_class=HTMLResponse)
@@ -155,18 +176,20 @@ async def variables(request: Request):
             'col_three': 'Shell variables can be either local or environment'
         }
     }
+
     resp: dict = {
         "request": request,
         "title": "Variables",
         "description": '"Types of Variables"',
         "next": "/path-environ",
-        "previous": "/config-files",
-        "header_one": "Type",
-        "header_two": "Description",
-        "header_three": "Example",
-        "items": bullets
+        "previous": await get_referer(request),
+        "thead_one": "Type",
+        "thead_two": "Description",
+        "thead_three": "Example",
+        "items": bullets,
     }
-    return templates.TemplateResponse(f'{overview_prefix}/variables.html', resp)
+
+    return templates.TemplateResponse(f'{overview_prefix}/{three_column_table}', resp)
 
 
 @app.get("/path-environ", response_class=HTMLResponse)
@@ -177,15 +200,17 @@ async def environ(request: Request):
         "Common and easy to modify",
         "Extremely important to the system as a whole"
     ]
+
     resp: dict = {
         "request": request,
         "title": "PATH Environment Variable",
         "description": '"PATH Environment Variable"',
         "next": "/epoch",
-        "previous": "/variables",
+        "previous": await get_referer(request),
         "items": bullets,
     }
-    return templates.TemplateResponse(f'{overview_prefix}/environ.html', resp)
+
+    return templates.TemplateResponse(f'{overview_prefix}/{bullet_slide}', resp)
 
 
 @app.get("/epoch", response_class=HTMLResponse)
@@ -195,28 +220,46 @@ async def epoch(request: Request):
         "Referred to as UNIX time, POSIX time, EPOCH time",
         "32-bit signed integer"
     ]
+
     resp: dict = {
         "request": request,
-        "title": "What is EPOCH?",
+        "title": "What is the EPOCH?",
         "description": '"EPOCH"',
         "next": "/qanda",
-        "previous": "/path-environ",
-        "items": bullets
+        "previous": await get_referer(request),
+        "items": bullets,
     }
-    return templates.TemplateResponse(f'{overview_prefix}/epoch.html', resp)
+
+    return templates.TemplateResponse(f'{overview_prefix}/{bullet_slide}', resp)
 
 
 @app.get("/qanda", response_class=HTMLResponse)
-async def thanks(request: Request):
+async def qanda(request: Request):
     resp: dict = {
         "request": request,
         "title": "Q & A",
+        "title_override_super_large_text": True,
         "descriptions": '"Questions and Answers"',
-        "next": "/",
-        "previous": "/variables"
+        "next": "/cli",
+        "previous": await get_referer(request),
     }
+
     return templates.TemplateResponse(f'{overview_prefix}/qanda.html', resp)
 
 
+@app.get("/cli", response_class=HTMLResponse)
+async def cli(request: Request):
+    resp: dict = {
+        "request": request,
+        "title": "Code Block",
+        "descriptions": '"Code Example"',
+        "code": "echo y | docker container prune",
+        "next": "/title",
+        "previous": await get_referer(request),
+    }
+
+    return templates.TemplateResponse(f'{cli_prefix}/title.html', resp)
+
+
 if __name__ == '__main__':
-    uvicorn.run(app)
+    uvicorn.run("main:app")
